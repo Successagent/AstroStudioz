0f49c35e299afdc14657a8deb83462e218fc6ea1
Success Agent <miesineagent@gmail.com>
2023-04-11 09:00:03 +0100

    updated

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -15,7 +15,7 @@ import Banner from "../../components/Banner";
 import Aos from "aos";
 import "aos/dist/aos.css";
 
-function About({}) {
+function About() {
   useEffect(() => {
     Aos.init({ duration: 2000 });
   }, []);
