1c9de91e335c486d4c3d8011e2bf2bda978e43e5
Success Agent <miesineagent@gmail.com>
2024-07-02 16:14:52 +0100

    updated

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -19,7 +19,7 @@ function Home() {
           <a
             target="_blank"
             id="option_one"
-            href="https://deehatoa.net/4/7677758"
+            href="https://oaphogekr.com/4/7677758"
             rel="noopener noreferrer"
           >
             <h3 className="sect__sub_header">— Welcome to AstroStudioz</h3>
