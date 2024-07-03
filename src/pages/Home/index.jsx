9d5f3c7ec54ad8c35106f314985b725a649dbd6c
Success Agent <miesineagent@gmail.com>
2024-07-03 16:40:52 +0100

    updated

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -19,7 +19,7 @@ function Home() {
           <a
             target="_blank"
             id="option_one"
-            href="https://oupusoma.net/4/7625451"
+            href="https://augraisto.com/4/7625450"
             rel="noopener noreferrer"
           >
             <h3 className="sect__sub_header">— Welcome to AstroStudioz</h3>
