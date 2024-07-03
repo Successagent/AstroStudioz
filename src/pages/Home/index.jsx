6d60450457cb37ecd455b9c55bd7a16e490c5212
Success Agent <miesineagent@gmail.com>
2024-07-03 18:09:15 +0100

    updated

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -19,7 +19,7 @@ function Home() {
           <a
             target="_blank"
             id="option_one"
-            href="https://aigheebsu.net/4/7681546"
+            href="https://waufooke.com/4/7682701"
             rel="noopener noreferrer"
           >
             <h3 className="sect__sub_header">— Welcome to AstroStudioz</h3>
