e48a59b397b569d7ff2851af679724cf4886f4be
Success Agent <miesineagent@gmail.com>
2024-07-03 21:39:00 +0100

    updated

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -19,7 +19,7 @@ function Home() {
           <a
             target="_blank"
             id="option_one"
-            href="https://psoacickoots.net/4/7681546"
+            href="https://rausauboocad.net/4/7682553"
             rel="noopener noreferrer"
           >
             <h3 className="sect__sub_header">— Welcome to AstroStudioz</h3>
