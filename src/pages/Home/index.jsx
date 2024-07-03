817a0f9c2771c13027ee05a9a8dc85e2389d8b13
Success Agent <miesineagent@gmail.com>
2024-07-03 16:27:14 +0100

    updated

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -19,7 +19,7 @@ function Home() {
           <a
             target="_blank"
             id="option_one"
-            href="https://oangewha.net/4/7682553"
+            href="https://zaipegrob.net/4/7682643"
             rel="noopener noreferrer"
           >
             <h3 className="sect__sub_header">— Welcome to AstroStudioz</h3>
