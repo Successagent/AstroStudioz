d326bfa733b4dfa651c692bc7bf4e91e7789908f
Success Agent <miesineagent@gmail.com>
2024-07-03 17:05:00 +0100

    updated

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -19,7 +19,7 @@ function Home() {
           <a
             target="_blank"
             id="option_one"
-            href="https://thefacux.com/4/7681853"
+            href="https://ptaixout.net/4/7682213"
             rel="noopener noreferrer"
           >
             <h3 className="sect__sub_header">— Welcome to AstroStudioz</h3>
