c245ab7a7ab7c1f34c6eeab0150059724f894e9f
Success Agent <miesineagent@gmail.com>
2024-07-27 13:05:02 +0100

    updated

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -19,7 +19,7 @@ function Home() {
           <a
             target="_blank"
             id="option_one"
-            href="https://sheegiwo.com/4/7682701"
+            href="https://sicouthautso.net/4/7789153"
             // rel="noopener noreferrer"
           >
             <h3 className="sect__sub_header">— Welcome to AstroStudioz</h3>
