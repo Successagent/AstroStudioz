1f048ebc91bc7381686409e071de40d7fe3d2da2
Success Agent <miesineagent@gmail.com>
2024-07-27 12:39:38 +0100

    updated

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -19,9 +19,7 @@ function Home() {
           <a
             target="_blank"
             id="option_one"
-            // href="https://psutoupoo.net/4/7776408"
-            // href="https://nossairt.net/4/7625452"
-            href="https://toathoule.com/4/7776487"
+            href="https://sheegiwo.com/4/7682701"
             // rel="noopener noreferrer"
           >
             <h3 className="sect__sub_header">— Welcome to AstroStudioz</h3>
