2115be251033c7e565cd0384ceef09460558d061
Success Agent <miesineagent@gmail.com>
2025-02-04 14:02:03 +0100

    updated

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -61,6 +61,7 @@ function Home() {
             </div>
             <div className="sect__text--col">
               <a
+              id=""
                 href="https://cohereproceedingsspark.com/w3ratsn2rb?key=43a1b6b92453d5d71d3e8528893d7a05"
                 rel="noopener noreferrer"
               >
